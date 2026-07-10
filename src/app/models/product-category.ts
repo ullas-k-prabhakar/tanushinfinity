508322a38963af53dcb53961718fd825bca62f96
ullas-k-prabhakar <39984854+ullas-k-prabhakar@users.noreply.github.com>
2026-07-10 21:13:52 +0530

    Match Cloudflare Pages output directory

---
+++ src/app/models/product-category.ts
@@ -0,0 +1,10 @@
+export interface ProductCategory {
+  id: number;
+  name: string;
+  slug: string;
+  description: string;
+  image: string;
+  sampleProducts: string[];
+  brands: string[];
+  specifications: string[];
+}
